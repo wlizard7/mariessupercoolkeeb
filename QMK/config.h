1fad95ea3496d3345b247edf44e052dfe41f9438
wlizard7 <wlizardaot@gmail.com>
2026-03-29 19:09:28 -0700

    Keyboard Firmware

---
+++ QMK/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 13
+
+#define ENCODERS 2
+#define ENCODER_RESOLUTION 4
+#define ENCODER_A_PINS { GP19, GP20 }
+#define ENCODER_B_PINS { GP27, GP28 }
